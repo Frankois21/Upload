264c96020871778a4b296fa42717fd001f6bef59
Frankois21 <francois.ponceau@gmail.com>
2017-05-30 15:20:21 +0200

    upload

--- index.php
+++ index.php
@@ -12,42 +12,39 @@
 
 </head>
 <body>
-<form method="POST" action="index.php" enctype="multipart/form-data">
-    Fichier : <input type="file" name="avatar[]" multiple />
-    <input type="submit" name="envoyer" value="Envoyer le fichier">
+<div class="container-fluid">
+<form method="POST" action="" enctype="multipart/form-data">
+    Fichier : <input type="file" name="avatar[]" multiple="multiple"/>
+    <input type="submit" name="envoyer" value="Envoyer le fichier"/>
 </form>
 
 
-</body>
-</html>
-
-
+    <div class="row">
 
 <?php
+
+$extensions = array('.jpg', '.png', '.gif');
+
 if(isset($_POST['suppr'])){
     unlink($_POST['path']);
 }
 
+$dossier = 'uploads/';
+
 if(isset($_POST['envoyer'])) {
-    $dossier = 'uploads/';
+
+
     $taille_maxi = 10000000;
-    $extensions = array('.jpg', '.png', '.gif');
-
     $nbfichiersEnvoyes = count($_FILES['avatar']['name']);
-
-
     for ($i = 0; $i < $nbfichiersEnvoyes; $i++) {
 
         $fichier = basename($_FILES['avatar']['name'][$i]);
         $fichier_temp = $_FILES['avatar']['tmp_name'][$i];
         $taille = filesize($_FILES['avatar']['tmp_name'][$i]);
         $extension = strrchr($_FILES['avatar']['name'][$i], '.');
-
         if (!in_array($extension, $extensions)) $erreur = '<span class="non">Vous devez uploader le fichier ' . $i . ' de type PNG, JPEG ou JPG<br></span>';
         if ($taille > $taille_maxi) $erreur = '<span>Le fichier' . $i . ' est trop gros...</span>';
         if (!isset($erreur)) {
-            $fichier = strtr($fichier, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-            $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
             $fichier = 'image' . '' . (rand(0, 1000000)) . $extension;
             if (move_uploaded_file($fichier_temp, $dossier . $fichier)) echo '<span class="okdac">Upload ' . $i . ' effectué avec succès !<br></span>';
             else echo '<span class="non">Echec de l\'upload ' . $i . ' !</span>';
@@ -55,40 +52,37 @@ if(isset($_POST['envoyer'])) {
         } else echo $erreur;
 
     }
-    ?>
-
-    <div class="container-fluid" >
-        <div class="row">
-            <div class="col-sm-6">
-    <?php
+}
     $images = scandir($dossier);
+    unset($images[0]);
+    unset($images[1]);
+
     foreach ($images as $image)
-
     {
-        if($image !='.' && $image!='..'){
 
-
-            echo '
+            echo '<div class="col-sm-3">
                     <div class="thumbnail img-responsive" >
                       <img src="uploads/'.$image .'" class="upload">
                           <div class="caption">
                             <h3>'.$image.'</h3> 
-                            <form method="POST" action="index.php">
+                            <form method="POST">
                             <input type="hidden" value="'.$dossier.'/'.$image.'" name="path">
-                            <input type="submit" value= "supprimer :'.$image.'" class="btn btn-danger" role="button" 
+                            <input type="submit" value= "supprimer" class="btn btn-danger"
                             name="suppr" ></form>
                           </div>
+                    </div>
                     </div>';
 
-        }
     }
-}
+
+
 ?>
-            </div>
+
     </div>
   </div>
 
-
+</body>
+</html>
 
 
 
